462c3342f1781cacc536c96f3df21deb0d1037a2
Adi gensher <adigincher@mail.tau.ac.il>
2022-07-06 13:39:44 +0300

    First edition of SETUP file

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Apply CLAHE algorithem to improve image contrast '
+LONG_DESCRIPTION = 'A package that allows the user to choose different paramaters for CLAHE algorithem and compare differences'
+
+# Setting up
+setup(
+    name="CLAHEckethon",
+    version=VERSION,
+    author="The best team in the world ever",
+    author_email="<adigincher@mail.tau.ac.il>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    keywords=['python', 'MRI', 'CLAHE', 'contrast'],
+    classifiers=[
+        "Development Status :: Done",
+        "Intended Audience :: Students",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
