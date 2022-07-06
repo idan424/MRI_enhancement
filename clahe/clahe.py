daa0ed77de1d7c09f75c8de5646437be381b6a4e
Idan Lavi <idan424@gmail.com>
2022-07-06 12:03:42 +0300

    added a check for int8 and conversion if needed

--- clahe/clahe.py
+++ clahe/clahe.py
@@ -1,15 +1,30 @@
 import numpy as np
-from matplotlib import pyplot as plt 
+from matplotlib import pyplot as plt
+import matplotlib.image as mpimg
+
 import cv2
 
-#clipLimit	- Threshold for contrast limiting.
-#tileGridSize - Size of grid for histogram equalization. Input image will be divided into equally sized rectangular tiles. tileGridSize defines the number of tiles in row and column.
 
-def get_clahe_image(img, clipLimit=2.0, tileGridSize=(8,8)):
-    clahe = cv2.createCLAHE(clipLimit , tileGridSize)
-    cl_img = clahe.apply(img)
+def get_clahe_image(img: np.ndarray, clipLimit=2.0, tileGridSize=(8, 8)):
+    """
+
+    :param img: Input image, np.array of size (256,256)
+    :param clipLimit: Threshold for contrast limiting.
+    :param tileGridSize: Size of grid for histogram equalization.
+                         Input image will be divided into equally sized
+                         rectangular tiles. tileGridSize defines the number
+                         of tiles in row and column.
+    :return: clahe image, np.array of size (256,256)
+    """
+    gray_image = img.astype(np.uint8) if img.dtype != np.uint8  else img
+
+    clahe = cv2.createCLAHE(clipLimit, tileGridSize)
+    cl_img = clahe.apply(gray_image)
+
     return cl_img
-    
+
+
 if __name__ == '__main__':
-    img = ''
-    print(get_clahe_image(img))
+    img = mpimg.imread('../images/glioblastoma-84-coronal.jpg').mean(axis=2)
+    enc = get_clahe_image(img)
+    plt.imshow(enc)
