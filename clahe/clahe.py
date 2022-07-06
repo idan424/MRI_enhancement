badd250fd60e5c366bbb5b658e910f54adc5e321
Idan Lavi <idan424@gmail.com>
2022-07-06 12:29:13 +0300

    added some viewing functionality for easy use

--- clahe/clahe.py
+++ clahe/clahe.py
@@ -24,7 +24,38 @@ def get_clahe_image(img: np.ndarray, clipLimit=2.0, tileGridSize=(8, 8)):
     return cl_img
 
 
+def plot_diff(img, enc):
+    """
+    This function shows the image befor and after CLAHE
+    :param img: the original image, np.ndarray
+    :param enc: the enhanced image, np.ndarray
+    """
+
+    fig, axs = plt.subplots(1,2)
+
+    axs[0].imshow(img, cmap='gray')
+    axs[0].set_title('Original image')
+
+    axs[1].imshow(enc, cmap='gray')
+    axs[1].set_title('Enhanced image')
+    for ax in axs:
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+def process_and_show(img):
+    """
+
+    :param img: The image to process and present the, np.ndarray
+    :return: clahe output image, np.ndarray
+    """
+
+    enc = get_clahe_image(img)
+    plot_diff(img, enc)
+
+    return enc
+
+
 if __name__ == '__main__':
     img = mpimg.imread('../images/glioblastoma-84-coronal.jpg').mean(axis=2)
-    enc = get_clahe_image(img)
-    plt.imshow(enc)
+    enc = process_and_show(img)
+
