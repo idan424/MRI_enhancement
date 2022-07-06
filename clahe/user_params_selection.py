6a343a17f6d56cd240b7caec2e75d83dab5a2fb3
Adi gensher <adigincher@mail.tau.ac.il>
2022-07-06 18:49:55 +0300

    Doctring to user_define function and documentaion to the main

--- clahe/user_params_selection.py
+++ clahe/user_params_selection.py
@@ -1,5 +1,11 @@
 
 def user_params_selection(): 
+    """
+    This function asks the user to define that algorithm parameters
+
+    :param :none
+    :return: float,tuple(int,int) : answers of the user
+    """
     clipLimit = input("Choose Threshold for contrast limiting(Default = 2.0):\n")
     clipLimit = float(clipLimit)
     n = input("Size of grid for histogram equalization.\nDefines the number of tiles in row and column(Default =8 => (8,8)):\n")
--- main.py
+++ main.py
@@ -8,13 +8,17 @@ from clahe.clahe import get_clahe_image
 
 
 if __name__ == '__main__':
+    #In order to run on our module if it is the source file
     img_names = get_img_names()
 
     for img_name in img_names:
+        #run over all choosen images, aplly the pre-rocessing and the CLAHE algorithem 
+        
         img = np.array(Image.open(img_name))
         pp_img = process_image(img_name)
         enc_img = get_clahe_image(pp_img)
 
+        # show the results of the 3 processing stats of the image - Originale,after pre-processing,after CLAHE algorithem
         plot_diff(img, pp_img, enc_img, img_name.split('/')[-1])
 
     plt.show()
