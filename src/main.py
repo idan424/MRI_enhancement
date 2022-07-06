8f5f968335991d0d805737d0dd620ca1f36af862
Idan Lavi <idan424@gmail.com>
2022-07-06 18:01:28 +0300

    added user param choosing

--- src/main.py
+++ src/main.py
@@ -2,18 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
-from ui import get_img_names, plot_diff
+from ui import get_img_names, plot_diff, user_params_selection
 from src.preprocessing.preprocessing import process_image
 from src.clahe.clahe import get_clahe_image
 
 
 if __name__ == '__main__':
+    clipLimit, tileGridSize = user_params_selection()
     img_names = get_img_names()
 
     for img_name in img_names:
         img = np.array(Image.open(img_name))
         pp_img = process_image(img_name)
-        enc_img = get_clahe_image(pp_img)
+        enc_img = get_clahe_image(pp_img, clipLimit, tileGridSize)
 
         plot_diff(img, pp_img, enc_img, img_name.split('/')[-1])
 
--- src/ui.py
+++ src/ui.py
@@ -6,12 +6,38 @@ from tkinter import filedialog
 root = tk.Tk()
 root.withdraw()
 
+
 def get_img_names():
     '''
     This function asks from the user to choose files
     :return: a list of file names
     '''
     return list(filedialog.askopenfilenames(initialdir='images'))
+
+
+def user_params_selection():
+    '''
+    This function allows for user params input
+    :return: clipLimit, tileGridSize
+
+    '''
+    choose = input('Do you want to choose params?(Y/N)')
+    if choose.lower() == 'y':
+        try:
+            clipLimit = input("Choose Threshold for contrast limiting (Default = 2.0): ")
+            clipLimit = float(clipLimit)
+            n = input("Size of grid for histogram equalization." +
+                      "Defines the number of tiles in row and column (Default = (8,8)): ")
+            tileGridSize = (int(n), int(n))
+        except Exception as e:
+            print(e)
+            raise e
+    else:
+        print('using default values')
+        clipLimit = 2.0
+        tileGridSize = (8, 8)
+
+    return clipLimit, tileGridSize
 
 
 def plot_diff(img, pp_img, enc, title, names=None):
@@ -21,22 +47,24 @@ def plot_diff(img, pp_img, enc, title, names=None):
     :param enc: the enhanced image, np.ndarray
     """
     if names == None:
-        names = ['Original image','Processed image','Enhanced image']
-    fig, axs = plt.subplots(1,3)
+        names = ['Original image', 'Processed image', 'Enhanced image']
 
+    fig, axs = plt.subplots(1, 3)
     axs[0].imshow(img, cmap='gray')
+
     axs[0].set_title(names[0])
-
     axs[1].imshow(pp_img, cmap='gray')
+
     axs[1].set_title(names[1])
-
     axs[2].imshow(enc, cmap='gray')
-    axs[2].set_title(names[2])
 
+    axs[2].set_title(names[2])
     for ax in axs:
         ax.set_xticks([])
         ax.set_yticks([])
+
     plt.suptitle(title)
+
 
 if __name__ == '__main__':
     imgs = get_img_names()
