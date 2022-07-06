8d648216e6fb22022f86c3a1d77be4e45c82d5c8
Adi gensher <adigincher@mail.tau.ac.il>
2022-07-06 10:42:21 +0300

    get clahe image code - first edition

--- clahe/clahe.py
+++ clahe/clahe.py
@@ -1,2 +1,15 @@
-#check
+import numpy as np
+from matplotlib import pyplot as plt 
+import cv2
 
+#clipLimit	- Threshold for contrast limiting.
+#tileGridSize - Size of grid for histogram equalization. Input image will be divided into equally sized rectangular tiles. tileGridSize defines the number of tiles in row and column.
+
+def get_clahe_image(img, clipLimit=2.0, tileGridSize=(8,8)):
+    clahe = cv2.createCLAHE(clipLimit , tileGridSize)
+    cl_img = clahe.apply(img)
+    return cl_img
+    
+if __name__ == '__main__':
+    img = ''
+    print(get_clahe_image(img))
