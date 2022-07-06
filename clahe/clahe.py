2f09aa556b7dc6ceaaacfe4c9b1bc0c10489b4a3
Adi gensher <adigincher@mail.tau.ac.il>
2022-07-06 12:27:29 +0300

    Adding user selection function for parms

--- clahe/clahe.py
+++ clahe/clahe.py
@@ -5,11 +5,11 @@ import cv2
 #clipLimit	- Threshold for contrast limiting.
 #tileGridSize - Size of grid for histogram equalization. Input image will be divided into equally sized rectangular tiles. tileGridSize defines the number of tiles in row and column.
 
-def get_clahe_image(img, clipLimit=2.0, tileGridSize=(8,8)):
+def get_clahe_image(processed_img, clipLimit=2.0, tileGridSize=(8,8)):
     clahe = cv2.createCLAHE(clipLimit , tileGridSize)
-    cl_img = clahe.apply(img)
+    cl_img = clahe.apply(processed_img)
     return cl_img
     
 if __name__ == '__main__':
     img = ''
-    print(get_clahe_image(img))
+    print(get_clahe_image(processed_img))
