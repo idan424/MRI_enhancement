005b96a2b631d144325d7208a2016d5e4ea5b12e
Idan Lavi <idan424@gmail.com>
2022-07-07 14:12:48 +0300

    test file for preprocessing - tests are passed

--- src/tests/tests_prep.py
+++ src/tests/tests_prep.py
@@ -1,80 +1,72 @@
-import pytest
+import os
+import cv2
 
-from PIL import Image
 from skimage.metrics import peak_signal_noise_ratio
-from src.preprocessing.preprocessing import process_image
+from src.preprocessing.preprocessing import *
+from PIL import Image
+
+img_name = "../../images/glioblastoma-84-coronal.jpg"  # Choose the image you want to
+
+# Create gray same size image in order to check the final image mode
+img_gray = Image.new('RGB', (256, 256), color='red').convert("L")
+
+# load path to the smaller size image
+img_small = 'small_test_img.jpeg'
+# load path to the larger size image
+img_large = 'large_test_img.jpeg'
+
+# Create the test images if they don't exist
+if img_small not in os.listdir(os.getcwd()):
+    # Create image numpy array - black picture
+    small = np.zeros((256, 256))
+    # Save image
+    cv2.imwrite(img_small, small)
+
+if img_large not in os.listdir(os.getcwd()):
+    # Create image numpy array - black picture
+    large = np.zeros((512, 512))
+    # Save image
+    cv2.imwrite(img_large, large)
 
 
-# Create defined sample test images
-img_small = Image.new('RGB', (60, 30), color = 'red')
-img_large = Image.new('RGB', (500, 300), color = 'red')
-img_equal = Image.new('RGB', (256, 256), color = 'red')
-img_gray  = img_equal.convert("L")
-img_noisy = img_gray.effect_noise((256, 256), 200)  
-
-# Calculate psnr (peak signal noise ration) metrics for greyscale resized image, verus image with added noise
-psnr_noisy = peak_signal_noise_ratio(img_gray, img_noisy)
-
-# Create an image with gaussian noise of size 256x256 and standard deviation 200
-
-class TestPreprocessing:
+def test_original_image_resize():
     """
-    A group of tests to evaluate sucessful preprocessing
+    Test for proper resizing of image from the Database: img_name
+        
     """
+    processed_image = process_image(img_name)
+    assert processed_image.shape == (256, 256)
+
+
+def test_resize():
+    """
+    2 Tests for proper resizing of images: smaller, larger than final size
         
-    def test_resize(self):
-        """
-        3 Tests for proper resizing of images: smaller, larger, and equal size images
-        
-        """
-        processed_small = process_image(img_small)
-        assert processed_small.size == (256, 256)
-        processed_large = process_image(img_large)
-        assert processed_large.size == (256, 256)
-        processed_equal = process_image(img_equal)
-        assert processed_equal.size == (256, 256)
-        
-    def test_greyscale(self):
-        processed_gray = process_image(img_equal)
-        assert processed_gray.mode() == img_gray.mode() 
-        
-        # alternative, requires import numpy
-        # assert np.array(processed_gray).dtype() != np.uint8
-    
-    def test_denoised(self):
-        """
+    """
+    processed_small = process_image(img_small)
+    assert processed_small.shape == (256, 256)
+
+    processed_large = process_image(img_large)
+    assert processed_large.shape == (256, 256)
+
+
+def test_greyscale():
+    processed_gray = process_image(img_name)
+    assert len(processed_gray.shape) == 2
+
+
+def test_denoised():
+    """
         Tests whether the preprocessing sucessfully redoces noise
         by comparing histogram peak signal to noise ratios.
         The improved image should have a lower psnr
 
-        """
-        processed_noisy = process_image(img_noisy)
-        psnr_processed = peak_signal_noise_ratio(processed_noisy, img_noisy) 
-        assert psnr_processed < psnr_noisy 
-    
-    
-    
-    
-# if __name__ == "__main__":
-#     # pytest.main()
-#     unittest.main()
-#     tests = TestPreprocessing()
-#     methods = ["resize"]
-#     errors = []
+    """
+    img = Image.open(img_name)
+    newsize = (256, 256)
+    img_resized = img.resize(newsize)
+    pp_img = np.array(ImageOps.grayscale(img_resized)).astype(float)
+    processed_img = process_image(img_name)
+    psnr_processed = peak_signal_noise_ratio(pp_img.astype(int), processed_img)
 
-#     img_small = Image.new('RGB', (60, 30), color = 'red')
-#     img_large = Image.new('RGB', (500, 300), color = 'red')
-#     img_equal = Image.new('RGB', (256, 256), color = 'red')
-#     img_grey  = img_equal.convert("L")
-#     img_noisy = Image.effect_noise((256, 256), 200)    
-
-    # for method in methods:
-    #     try:
-    #         getattr(tests, "test_" + method)()
-    #     except AssertionError as e:
-    #         errors.append(f"Failed when testing method 'test_{method}': {e}")
-
-    # if len(errors) > 0:
-    #     print(errors)
-    # else:
-    #     print("Tests pass successfully.")
+    assert psnr_processed < np.inf
